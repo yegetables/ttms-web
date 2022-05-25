1e0a83e4d8dfcdb0ab969a56596edf7c95c086f3
ajian2002 <ajian020515@163.com>
2022-05-25 20:58:46 +0800

    add application.properties

--- web/src/main/java/com/xupt/controller/LoginController.java
+++ web/src/main/java/com/xupt/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.xupt.controller;
 
 import com.xupt.dao.UserMapper;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,10 +13,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 
   @Autowired private UserMapper userMapper;
+  @Autowired private RedisTemplate jedisTemplate;
 
   @RequestMapping("/getUserName")
   @ResponseBody
   public String getUserName(Integer id) {
     return userMapper.getUserName(id);
+  }
+
+  @RequestMapping("/login")
+  @ResponseBody
+  public String login(@RequestBody Map<String, String> map) {
+    if (map.containsKey("username") && map.containsKey("password")) {
+      return "success";
+    } else {
+      return "username or password is null  " + "you send[" + map.toString() + "]";
+    }
+  }
+
+  @RequestMapping("/count")
+  @ResponseBody
+  public String count(@RequestBody Map<String, String> map) {
+    if (map.containsKey("id")) {
+      return "" + Integer.valueOf(map.get("id")) + 1;
+    } else {
+      return "id is null  " + "you send[" + map.toString() + "]";
+    }
   }
 }
