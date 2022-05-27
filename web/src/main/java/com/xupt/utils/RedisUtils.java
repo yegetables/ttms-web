6e103a1ffbfed57177f4b3dcd0888869a24b14e6
XiAoRayL <1907402664@qq.com>
2022-05-27 17:38:57 +0800

    基本工具类的添加 MVC配置 以及密码登录接口的实现 User类的实现

---
+++ web/src/main/java/com/xupt/utils/RedisUtils.java
@@ -0,0 +1,49 @@
+package com.xupt.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+@Component
+public class RedisUtils {
+    @Autowired
+    RedisTemplate<String,Object> redisTemplate;
+    public String get(String key){
+        return (String)redisTemplate.opsForValue().get(key);
+    }
+    public  void set(String key,Object value){
+        redisTemplate.opsForValue().set(key,value);
+    }
+    public boolean set(String key, Object value, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.opsForValue().set(key,value,time,TimeUnit.SECONDS);
+            } else {
+                set(key, value);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public void setList(String key,Object value){
+        redisTemplate.opsForList().leftPush(key,value);
+    }
+    public List<Object> getAllList(String key){
+        List<Object> res=redisTemplate.opsForList().range(key,0,-1);
+        return res;
+    }
+    public boolean delete(String uid){
+        Boolean delete = redisTemplate.delete(uid);
+        return delete;
+    }
+    public void expire(String key,int day){
+        redisTemplate.expire(key,1,TimeUnit.DAYS);
+    }
+    public void lRemove(String key){
+        redisTemplate.opsForList().trim(key,1,0);
+    }
+}
